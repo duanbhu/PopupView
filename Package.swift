dc717f7934e4a295fb12e99bd582b6235501261b
dbh <310701836@qq.com>
2025-05-30 14:07:20 +0800

    feat: ios 最低版本

--- Package.swift
+++ Package.swift
@@ -5,17 +5,23 @@ import PackageDescription
 
 let package = Package(
     name: "PopupView",
+    platforms: [.iOS("13.0")],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PopupView",
             targets: ["PopupView"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/SwiftKickMobile/SwiftMessages.git", .upToNextMajor(from: "10.0.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PopupView"),
+            name: "PopupView", dependencies: [
+                .product(name: "SwiftMessages", package: "SwiftMessages")
+            ]),
         .testTarget(
             name: "PopupViewTests",
             dependencies: ["PopupView"]
